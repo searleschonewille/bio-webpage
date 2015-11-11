db6b8c26f26fe530e0f9b23d068e66c54669127e
Searle Schonewille <searleschonewille@yahoo.ca>
2015-11-10 19:42:52 -0500

    initial commit of spitfire-auj website

---
+++ scripts/simple.js
@@ -0,0 +1,24 @@
+$(document).ready();
+	$('#read-more-1').mouseleave(function() {
+		$('.content-about .show-more-1').hide();
+});
+
+	$('#read-more-1').on('click', function() {
+		$('.content-about .show-more-1').show();
+});
+
+	$('#read-more-2').mouseleave(function() {
+		$('.content-about .show-more-2').hide();
+});
+
+	$('#read-more-2').on('click', function() {
+		$('.content-about .show-more-2').show();
+});
+
+	$('#read-more-3').mouseleave(function() {
+		$('.content-about .show-more-3').hide();
+});
+
+	$('#read-more-3').on('click', function() {
+		$('.content-about .show-more-3').show();
+});
